8faee52c0506f8ad378c1d3577931cddbbff2bd7
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-02-22 12:47:17 -0500

    Add PropLoader.dll, EntityBase, and IRepositoryBase

---
+++ ServiceXpert.API.Domain/Abstractions/Interfaces/Repositories/IRepositoryBase.cs
@@ -0,0 +1,48 @@
+﻿using PropLoader;
+using ServiceXpert.API.Domain.Entities;
+
+namespace ServiceXpert.API.Domain.Abstractions.Interfaces.Repositories
+{
+    public interface IRepositoryBase<TEntityID, TEntity> where TEntity : EntityBase
+    {
+        string EntityID
+        {
+            get;
+            set;
+        }
+
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync();
+
+        void Attach(TEntity entity);
+
+        TEntity GetByID(TEntityID entityID, IncludeOptions<TEntity>? includeOptions = null);
+
+        Task<TEntity> GetByIDAsync(TEntityID entityID, IncludeOptions<TEntity>? includeOptions = null);
+
+        IEnumerable<TEntity> GetAll(IncludeOptions<TEntity>? includeOptions = null);
+
+        Task<IEnumerable<TEntity>> GetAllAsync(IncludeOptions<TEntity>? includeOptions = null);
+
+        void Add(TEntity entity);
+
+        Task AddAsync(TEntity entity);
+
+        void AddRange(params TEntity[] entities);
+
+        Task AddRangeAsync(params TEntity[] entities);
+
+        void AddRange(List<TEntity> entities);
+
+        Task AddRangeAsync(List<TEntity> entities);
+
+        void Update(TEntity entity);
+
+        void DeleteByID(TEntityID entityID);
+
+        Task DeleteByIDAsync(TEntityID entityID);
+
+        void Delete(TEntity entity);
+    }
+}
---
+++ ServiceXpert.API.Domain/Entities/EntityBase.cs
@@ -0,0 +1,9 @@
+﻿namespace ServiceXpert.API.Domain.Entities
+{
+    public abstract class EntityBase
+    {
+        public DateTime CreateDate { get; set; }
+
+        public DateTime ModifyDate { get; set; }
+    }
+}
