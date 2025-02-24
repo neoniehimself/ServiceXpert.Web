1afe6989244d6281b3c5c2201cac96aff6fd30c2
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-02-24 09:22:55 -0500

    Add Issue and IssuePreFix

---
+++ ServiceXpert.API.Domain/Entities/Issue.cs
@@ -0,0 +1,11 @@
+﻿namespace ServiceXpert.API.Domain.Entities
+{
+    public class Issue : EntityBase
+    {
+        public int IssueID { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+    }
+}
