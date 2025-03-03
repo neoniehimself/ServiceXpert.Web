9aee1c70e9f23b65d0bd940d33156ae0283afb37
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-03-03 12:51:21 -0500

    Add MVC Project

---
+++ ServiceXpert.Web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Mvc;
+using ServiceXpert.Web.Models;
+using System.Diagnostics;
+
+namespace ServiceXpert.Web.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        this.logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? this.HttpContext.TraceIdentifier });
+    }
+}
