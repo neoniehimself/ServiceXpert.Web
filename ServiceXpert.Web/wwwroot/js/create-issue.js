600b0b1d1bf442f889dfbcd75f488e764c7984ad
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-03-09 12:54:12 -0400

    create-issue.js

---
+++ ServiceXpert.Web/wwwroot/js/create-issue.js
@@ -0,0 +1,11 @@
+﻿$(document).on('click', '#btn-create-issue', function (e) {
+    e.preventDefault();
+    $.ajax({
+        type: 'GET',
+        url: 'Home/CreateIssue',
+        success: function (response) {
+            $('.modal-container').html(response);
+            $('#create-issue-modal').modal('show');
+        }
+    });
+});
--- ServiceXpert.Web/wwwroot/js/site.js
+++ ServiceXpert.Web/wwwroot/js/site.js
@@ -10,18 +10,6 @@
     });
 });
 
-$(document).on('click', '#btn-create-issue', function (e) {
-    e.preventDefault();
-    $.ajax({
-        type: 'GET',
-        url: 'Home/CreateIssue',
-        success: function (response) {
-            $('.modal-container').html(response);
-            $('#create-issue-modal').modal('show');
-        }
-    });
-});
-
 /* Bootstrap Theme Switching
 function toggleTheme() {
     const html = document.documentElement;
