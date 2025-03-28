a03bb106ee7d70b4fde2c083a5fe75a51f3d4df5
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-03-28 17:23:04 -0400

    fix navbar toggler

--- ServiceXpert.Web/Extensions/HtmlExtension.cs
+++ ServiceXpert.Web/Extensions/HtmlExtension.cs
@@ -6,16 +6,18 @@ namespace ServiceXpert.Web.Extensions
     {
         public static string IsActive(this IHtmlHelper htmlHelper, string controller, string action)
         {
-            // Get route data
             var routeData = htmlHelper.ViewContext.RouteData;
 
-            // Get controller and page
             var routeAction = routeData.Values["action"]!.ToString();
             var routeController = routeData.Values["controller"]!.ToString();
 
-            return (controller == routeController) &&
-                   (action == routeAction)
-                   ? "active" : "";
+            var routePath = htmlHelper.ViewContext.HttpContext.Request.Path.Value?.Trim('/').ToLower();
+
+            bool isActive = (controller.Equals(routeController?.ToLower(), StringComparison.OrdinalIgnoreCase) &&
+                             action.Equals(routeAction?.ToLower(), StringComparison.OrdinalIgnoreCase)) ||
+                            (controller.Equals(routePath, StringComparison.OrdinalIgnoreCase));
+
+            return isActive ? "active" : "";
         }
     }
 }
