43c2801d731a8a9276200566dc42ea13f746f742
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-04-29 14:25:07 -0400

    Update ModelBase Properties

--- ServiceXpert.Web/Models/ModelBase.cs
+++ ServiceXpert.Web/Models/ModelBase.cs
@@ -3,9 +3,11 @@ public abstract class ModelBase
 {
     protected readonly string dateFormat = "dd/MM/yy";
 
-    public DateTime CreateDate { get; set; }
+    public DateTime? CreateDate { get; set; }
 
-    public string CreateDateFormatted => this.CreateDate.ToString(this.dateFormat);
+    public string CreateDateFormatted => this.CreateDate != null
+        ? this.CreateDate.Value.ToString(this.dateFormat)
+        : string.Empty;
 
     public DateTime? ModifyDate { get; set; }
 
