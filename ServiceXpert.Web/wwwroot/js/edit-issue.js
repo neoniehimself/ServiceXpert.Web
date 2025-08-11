190a7aa44c1522dcfd11cfd072ac9bb4eb9d82e4
NeonieQuell <neoniequell.ponce@yahoo.com>
2025-08-11 16:14:09 -0400

    edit issue save

---
+++ ServiceXpert.Web/wwwroot/js/edit-issue.js
@@ -0,0 +1,24 @@
+﻿$('#edit-issue-form').submit(function (e) {
+    e.preventDefault();
+
+    if (window.confirm('Do you want to save your changes?')) {
+        var issueKey = $('#issue-key-label').text();
+        $.ajax({
+            type: 'PUT',
+            url: `/Issues/${issueKey}/Edit`,
+            data: new FormData($(this)[0]),
+            cache: false,
+            processData: false,
+            contentType: false,
+            dataType: 'JSON',
+            success: function (response) {
+                // If status code is in 200, then operation was successful
+                if (response.statusCode >= 200 && response.statusCode <= 299) {
+                    window.location.href = `/Issues/${issueKey}`;
+                }
+            }
+        });
+    } else {
+        return false;
+    }
+});
