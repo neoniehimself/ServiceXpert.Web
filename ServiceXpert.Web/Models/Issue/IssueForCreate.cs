89b41abc7afaa0e899838756f1f37f14458b2b06
neoniehimself <neoniequell.ponce@yahoo.com>
2025-09-19 02:42:56 +0800

    Add Assignee in Create Issue Modal

--- ServiceXpert.Web/Models/Issue/IssueForCreate.cs
+++ ServiceXpert.Web/Models/Issue/IssueForCreate.cs
@@ -4,11 +4,13 @@ namespace ServiceXpert.Web.Models.Issue;
 public class IssueForCreate : ModelBaseForCreate
 {
     [Required]
-    public required string Name { get; set; }
+    public string Name { get; set; }
 
     public string? Description { get; set; }
 
     public int IssueStatusId { get; set; } = (int)Enums.IssueStatus.New;
 
     public int IssuePriorityId { get; set; } = (int)Enums.IssuePriority.Low;
+
+    public Guid? AssigneeId { get; set; }
 }
--- ServiceXpert.Web/ViewModels/CreateIssueViewModel.cs
+++ ServiceXpert.Web/ViewModels/CreateIssueViewModel.cs
@@ -1,10 +1,15 @@
-﻿namespace ServiceXpert.Web.ViewModels;
+﻿using ServiceXpert.Web.Models.AspNetUserProfile;
+
+namespace ServiceXpert.Web.ViewModels;
 public class CreateIssueViewModel
 {
     public Dictionary<int, string> IssuePriorities { get; set; }
 
+    public ICollection<AspNetUserProfile> Users { get; set; }
+
     public CreateIssueViewModel()
     {
         this.IssuePriorities = [];
+        this.Users = [];
     }
 }
